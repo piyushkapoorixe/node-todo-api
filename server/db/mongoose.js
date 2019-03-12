8f882dab06106f4cfa75ba4ba15d46ca3b5ad3a7
Piyush <piyushkapoorixe@gmail.com>
2019-03-12 14:20:19 +0530

    Updation (Patch) route added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,8 +7,8 @@ mongoose.Promise = global.Promise;  // importing inbuilt Promise lib from npm
 // const uri = "mongodb+srv://piyushkapoor:piyushkapoor@cluster0-xlkbt.mongodb.net/test?retryWrites=true";
 
 // uncomment the following 2 lines of code for heroku database usage by mlab
- const uri = "mongodb://piyush:kap00r@ds121495.mlab.com:21495/piyush";
- mongoose.connect(uri);
+const uri = "mongodb://piyush:kap00r@ds121495.mlab.com:21495/piyush";
+mongoose.connect(uri);
 
 //uncomment the following line of code for localhost usage
  //mongoose.connect('mongodb://localhost:27017/TodoApp');
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
@@ -64,6 +65,31 @@ app.delete('/todos/:todoid', (req, res) => {
     }
 
     Todomongoose.findByIdAndRemove(todoid).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:todoid', (req, res) => {
+    var todoid = req.params.todoid;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(todoid)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todomongoose.findByIdAndUpdate(todoid, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
